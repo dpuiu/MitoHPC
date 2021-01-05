c61f4f4cf646bb8c39cdd8c543d6aba5c6838f49
dpuiu <dpuiu@jhu.edu>
2021-01-05 15:52:04 -0500

    ...

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,9 +1,10 @@
-#!/bin/bash -eux
+#!/bin/bash -e
 
 SDIR=`dirname $0`        # script directory
 
 export SDIR=`readlink -f $SDIR`
-export PATH=$SDIR:$PATH
+export PATH=$SDIR:$SDIR/../bin/:$PATH
+echo $PATH
 
 export JDIR=`readlink -f $SDIR/../java`
 export RDIR=`readlink -f $SDIR/../RefSeq`
