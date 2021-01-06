f3adeafc67c9828d20f7cf63f5bdc4d5287b4f90
dpuiu <dpuiu@jhu.edu>
2021-01-06 14:01:57 -0500

    ...

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -e
 
 SDIR=`dirname $0`        # script directory
-
 export SDIR=`readlink -f $SDIR`
-export PATH=$SDIR:$SDIR/../bin/:$PATH
-echo $PATH
-
+export HDIR=`readlink -f $SDIR/..`
+export BDIR=`readlink -f $SDIR/../bin`
 export JDIR=`readlink -f $SDIR/../java`
 export RDIR=`readlink -f $SDIR/../RefSeq`
+
+export PATH=$SDIR:$BDIR:$PATH
+echo $PATH
 
 export SH="sh"                 # bash, sbatch(SLURM), qsub(SGE,PBS)
 
--- scripts/install_prerequisites.sh
+++ scripts/install_prerequisites.sh
@@ -1,89 +1,120 @@
 #!/bin/sh -eux
 
-mkdir -p prerequisites/ bin/ java/
+SDIR=`dirname $0`	 # script directory
+export SDIR=`readlink -f $SDIR`
+export HDIR=`readlink -f $SDIR/..`
+export BDIR=`readlink -f $SDIR/../bin`
+export JDIR=`readlink -f $SDIR/../java`
+export RDIR=`readlink -f $SDIR/../RefSeq`
+
+mkdir -p $HDIR/prerequisites/ $BDIR/ $JDIR/
 
 #############
-cd prerequisites/
+cd $HDIR/prerequisites/
 
-wget https://netactuate.dl.sourceforge.net/project/bio-bwa/bwa-0.7.17.tar.bz2
-tar -xjvf bwa-0.7.17.tar.bz2
-cd bwa-0.7.17
-make ; cp bwa ../../bin/
-cd -
+if [ ! -s $BDIR/bwa ] ; then
+  wget https://netactuate.dl.sourceforge.net/project/bio-bwa/bwa-0.7.17.tar.bz2
+  tar -xjvf bwa-0.7.17.tar.bz2
+  cd bwa-0.7.17
+  make ; cp bwa $BDIR/
+  cd -
+fi
 
-wget https://github.com/samtools/samtools/releases/download/1.11/samtools-1.11.tar.bz2
-tar -xjvf samtools-1.11.tar.bz2
-cd samtools-1.11
-#make ; cp samtools ../../bin/ ; cd -
-./configure --prefix=$PWD/../../; make ; make test; make install
-cd -
+if [ ! -s $BDIR/samtools ] ; then
+  wget https://github.com/samtools/samtools/releases/download/1.11/samtools-1.11.tar.bz2
+  tar -xjvf samtools-1.11.tar.bz2
+  cd samtools-1.11
+  ./configure --prefix=$HDIR/ ; make ; make test; make install
+  cd -
+fi
 
-wget https://github.com/samtools/bcftools/releases/download/1.11/bcftools-1.11.tar.bz2
-tar -xjvf  bcftools-1.11.tar.bz2
-cd bcftools-1.11
-#make ; cp bcftools ../../bin/
-./configure --prefix=$PWD/../../; make ; make test; make install
-cd -
+if [ ! -s $BDIR/bcftools ] ; then
+  wget https://github.com/samtools/bcftools/releases/download/1.11/bcftools-1.11.tar.bz2
+  tar -xjvf  bcftools-1.11.tar.bz2
+  cd bcftools-1.11
+  ./configure --prefix=$HDIR/ ; make ; make test; make install
+  cd -
+fi
 
-wget https://github.com/samtools/htslib/releases/download/1.11/htslib-1.11.tar.bz2
-tar -xjvf htslib-1.11.tar.bz2
-cd htslib-1.11
-#make; cp tabix ../../bin/
-./configure --prefix=$PWD/../../; make ; make test; make install
-cd -
+if [ ! -s $BDIR/tabix ] ; then
+  wget https://github.com/samtools/htslib/releases/download/1.11/htslib-1.11.tar.bz2
+  tar -xjvf htslib-1.11.tar.bz2
+  cd htslib-1.11
+  ./configure --prefix=$HDIR/ ; make ; make test; make install
+  cd -
+fi
 
-wget https://github.com/GregoryFaust/samblaster/releases/download/v.0.1.26/samblaster-v.0.1.26.tar.gz
-tar -xzvf samblaster-v.0.1.26.tar.gz
-cd samblaster-v.0.1.26
-make ; cp samblaster ../../bin/
-cd -
+if [ ! -s $BDIR/samblaster ] ; then
+  wget https://github.com/GregoryFaust/samblaster/releases/download/v.0.1.26/samblaster-v.0.1.26.tar.gz
+  tar -xzvf samblaster-v.0.1.26.tar.gz
+  cd samblaster-v.0.1.26
+  make ; cp samblaster $BDIR/
+  cd -
+fi
 
-wget https://github.com/vcftools/vcftools/releases/download/v0.1.16/vcftools-0.1.16.tar.gz
-tar -xzvf vcftools-0.1.16.tar.gz
-cd vcftools-0.1.16
-#./configure; make; cp src/cpp/vcftools ../../bin/
-./configure --prefix=$PWD/../../; make ; make test; make install
-cd -
+if [ ! -s $BDIR/vcftools ] ; then
+  wget https://github.com/vcftools/vcftools/releases/download/v0.1.16/vcftools-0.1.16.tar.gz
+  tar -xzvf vcftools-0.1.16.tar.gz
+  cd vcftools-0.1.16
+  ./configure --prefix=$HDIR/ ; make ; make install
+  cd -
+fi
 
-wget https://github.com/nferraz/st/archive/v1.1.4.tar.gz -O st-1.1.4.tar.gz
-tar -xzvf st-1.1.4.tar.gz
-cd st-1.1.4
-perl ./Makefile.PL INSTALL_BASE=../../ ; make ; make test ; make install
-cd -
+if [ ! -s $BDIR/st ] ; then
+  wget https://github.com/nferraz/st/archive/v1.1.4.tar.gz -O st-1.1.4.tar.gz
+  tar -xzvf st-1.1.4.tar.gz
+  cd st-1.1.4
+  perl ./Makefile.PL INSTALL_BASE=$HDIR/ ; make ; make test ; make install
+  cd -
+fi
 
-wget https://github.com/arq5x/bedtools2/releases/download/v2.29.2/bedtools-2.29.2.tar.gz
-tar -xzvf bedtools-2.29.2.tar.gz
-cd bedtools2/
-#make ; cp -i bin/bedtools  ../bin/
-make install prefix=../..
-cd -
+if [ ! -s $BDIR/bedtools ] ; then
+  wget https://github.com/arq5x/bedtools2/releases/download/v2.29.2/bedtools-2.29.2.tar.gz
+  tar -xzvf bedtools-2.29.2.tar.gz
+  cd bedtools2/
+  make install prefix=$HDIR/
+  cd -
+fi
 
-wget https://github.com/OpenGene/fastp/archive/v0.20.1.tar.gz -O fastp-0.20.1.tar.gz
-tar -xzvf fastp-0.20.1.tar.gz
-cd fastp-0.20.1/
-make ; cp fastp ../../bin/
-cd -
+if [ ! -s $BDIR/fastp ] ; then
+  wget https://github.com/OpenGene/fastp/archive/v0.20.1.tar.gz -O fastp-0.20.1.tar.gz
+  tar -xzvf fastp-0.20.1.tar.gz
+  cd fastp-0.20.1/
+  make ; cp fastp $BDIR/
+  cd -
+fi
 
 ################### 
+if [ ! -s $JDIR/picard.jar ] ; then
+  wget https://github.com/broadinstitute/picard/releases/download/2.23.8/picard.jar
+  cp picard.jar $JDIR/
+fi
 
-wget https://github.com/broadinstitute/picard/releases/download/2.23.8/picard.jar
-cp picard.jar ../java/
+if [ ! -s $JDIR/gatk.jar ] ; then
+  wget https://github.com/broadinstitute/gatk/releases/download/4.1.9.0/gatk-4.1.9.0.zip
+  unzip gatk-4.1.9.0.zip
+  cp gatk-4.1.9.0/gatk-package-4.1.9.0-local.jar $JDIR/gatk.jar
+fi
 
-wget https://github.com/broadinstitute/gatk/releases/download/4.1.9.0/gatk-4.1.9.0.zip
-unzip gatk-4.1.9.0.zip
-cp gatk-4.1.9.0/gatk-package-4.1.9.0-local.jar ../java/gatk.jar
+if [ ! -s $JDIR/haplogrep.jar ] ; then
+  wget https://github.com/seppinho/haplogrep-cmd/releases/download/v2.2.9/haplogrep.zip
+  unzip haplogrep.zip
+  cp haplogrep.jar $JDIR/
+fi
 
-wget https://github.com/seppinho/haplogrep-cmd/releases/download/v2.2.9/haplogrep.zip
-unzip haplogrep.zip
-cp haplogrep.jar ../java/
+if [ ! -s $JDIR/mutserve.jar ] ; then
+  wget https://github.com/seppinho/mutserve/releases/download/v1.3.4/mutserve-1.3.4.jar
+  cp mutserve-1.3.4.jar $JDIR/mutserve.jar
+  cd ../
+fi
 
-wget https://github.com/seppinho/mutserve/releases/download/v1.3.4/mutserve-1.3.4.jar
-cp mutserve-1.3.4.jar ../java/mutserve.jar
-cd ../
-
+cd -
 #################
 
-cd RefSeq/
-#wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/GRCh38_reference_genome/GRCh38_full_analysis_set_plus_decoy_hla.fa -O hs38DH.fa
-../bin/samtools faidx hs38DH.fa
-cd -
+if [ ! -s $RDIR/hs38DH.fa ] ; then
+  cd $RDIR
+  wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/GRCh38_reference_genome/GRCh38_full_analysis_set_plus_decoy_hla.fa -O hs38DH.fa
+  $BDIR/samtools faidx hs38DH.fa
+  cd -
+fi
+
