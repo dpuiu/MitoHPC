265278480913a5b29b080bd9c12e27134b0f80b5
dpuiu <dpuiu@jhu.edu>
2022-03-09 12:47:02 -0500

    ...

--- scripts/download_prerequisites.sh
+++ scripts/download_prerequisites.sh
@@ -19,7 +19,7 @@ wget -N -c https://github.com/samtools/bcftools/releases/download/1.15/bcftools-
 wget -N -c https://github.com/samtools/htslib/releases/download/1.15/htslib-1.15.tar.bz2
 wget -N -c https://github.com/GregoryFaust/samblaster/releases/download/v.0.1.26/samblaster-v.0.1.26.tar.gz
 wget -N -c https://github.com/arq5x/bedtools2/releases/download/v2.30.0/bedtools-2.30.0.tar.gz
-wget http://opengene.org/fastp/fastp
+wget -N -c http://opengene.org/fastp/fastp
 wget -N -c https://github.com/freebayes/freebayes/releases/download/v1.3.6/freebayes-1.3.6-linux-amd64-static.gz
 wget -N -c https://github.com/broadinstitute/gatk/releases/download/4.2.5.0/gatk-4.2.5.0.zip
 wget -N -c https://github.com/seppinho/haplogrep-cmd/releases/download/v2.4.0/haplogrep.zip
