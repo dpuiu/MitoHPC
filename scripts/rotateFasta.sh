9450c0afa92441630f5b953e92baca1f08430bff
dpuiu <dpuiu@jhu.edu>
2022-10-31 11:51:53 -0400

    .

--- scripts/rotateFasta.sh
+++ scripts/rotateFasta.sh
@@ -9,7 +9,7 @@ test -s $S.fa
 test -s $S.fa.fai
 
 cat $S.fa.fai | perl -ane 'print "$F[0]\t",$F[1]-$ENV{E},"\t$F[1]\n$F[0]\t0\t",$F[1]-$ENV{E},"\n";' | \
-    bedtools getfasta -fi $S.fa -bed -  | grep -v ">" |  perl -ane 'BEGIN { print ">$ENV{S}\n" }  chomp; print ; END {print "\n"}' > $SE.fa
+    bedtools getfasta -fi $S.fa -bed -  | grep -v ">" |  perl -ane 'BEGIN { print ">$ENV{N}\n" }  chomp; print ; END {print "\n"}' > $SE.fa
 
 samtools faidx $SE.fa
 
