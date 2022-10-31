8dd2650951fd8f5e7edc54ad442eb6d7f6e43c4c
dpuiu <dpuiu@jhu.edu>
2022-10-31 10:43:33 -0400

    .

--- scripts/circFasta.sh
+++ scripts/circFasta.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/bash -eux
 
-export N=$1
-export S=$2
-export E=$3
-
-SE=$S.c$E
+export S=$1
+export E=$2
+SE=$3
 
 test -s $S.fa
 test -s $S.fa.fai
 
-samtools faidx $S.fa $S:1-$E | cat $S.fa - | grep -v ">" |  perl -ane 'BEGIN { print ">$ENV{N}\n" }  chomp; print ; END {print "\n"}'  > $SE.fa
+samtools faidx $S.fa $S:1-$E | cat $S.fa - | grep -v ">" |  perl -ane 'BEGIN { print ">$ENV{S}\n" }  chomp; print ; END {print "\n"}'  > $SE.fa
 
 bwa index $SE.fa -p $SE
 samtools faidx $SE.fa
