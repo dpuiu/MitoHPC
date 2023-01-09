bdee1ecf43ebd35e111be7af97611a6244fc9b07
Daniela Puiu <dpuiu@jhu.edu>
2023-01-09 09:29:10 -0500

    Update circFasta.sh

--- scripts/circFasta.sh
+++ scripts/circFasta.sh
@@ -13,4 +13,5 @@ samtools faidx $S.fa $S:1-$E | cat $S.fa - | grep -v ">" |  perl -ane 'BEGIN { p
 bwa index $SE.fa -p $SE
 samtools faidx $SE.fa
 
-
+rm -f $SE.dict
+java -jar $HP_JDIR/gatk.jar CreateSequenceDictionary --REFERENCE $SE.fa --OUTPUT $SE.dict
