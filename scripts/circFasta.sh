6f882813e989de7aa3761037221c0d0898f0dd59
Daniela Puiu <dpuiu@jhu.edu>
2023-02-16 14:45:55 -0500

    Update circFasta.sh

--- scripts/circFasta.sh
+++ scripts/circFasta.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+####################################
+#1: sequence id (ex: chrM)
+#2: sequence FASTA file prefix (ex: path/chrM)
+#3: extensio length (ex:300)
+#4: output files prefix
+####################################
+
+export N=$1
+export S=$2
+export E=$3
+SE=$4
+
 test -s $S.fa
 test -s $S.fa.fai
 
