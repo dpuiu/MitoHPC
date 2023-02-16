b63fc12a9bd44e131b37f0b64b3ff996fc081df6
Daniela Puiu <dpuiu@jhu.edu>
2023-02-16 14:49:07 -0500

    Update rotateFasta.sh

--- scripts/rotateFasta.sh
+++ scripts/rotateFasta.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+####################################
+#1: sequence id (ex: chrM)
+#2: sequence FASTA file prefix (ex: path/chrM)
+#3: extensio length (ex:300)
+#4: output files prefix
+####################################
+
 export N=$1
 export S=$2
 export E=$3
@@ -9,7 +16,7 @@ test -s $S.fa
 test -s $S.fa.fai
 
 cat $S.fa.fai | perl -ane 'print "$F[0]\t",$F[1]-$ENV{E},"\t$F[1]\n$F[0]\t0\t",$F[1]-$ENV{E},"\n";' | \
-    bedtools getfasta -fi $S.fa -bed -  | grep -v ">" |  perl -ane 'BEGIN { print ">$ENV{N}\n" }  chomp; print ; END {print "\n"}' > $SE.fa
+    bedtools getfasta -fi $S.fa -bed - -fo /dev/stdout  | grep -v ">" |  perl -ane 'BEGIN { print ">$ENV{N}\n" }  chomp; print ; END {print "\n"}' > $SE.fa
 
 samtools faidx $SE.fa
 
