7b1ba57641f82a37e363312e0585797b3ce1912e
Daniela Puiu <dpuiu@jhu.edu>
2023-03-03 14:01:47 -0500

    Update downsampleSam.sh

--- scripts/downsampleSam.sh
+++ scripts/downsampleSam.sh
@@ -3,23 +3,26 @@
 IN=$1                                                                                   # input bam prefix(including path)
 OUT=$2                                                                                  # output bam prefix(including path)
 MCOUNT=${3:-10}                                                                         # max number of reads starting at a certain pos
-R=${4:-"chrM chr1:629084-634672 chr1:76971223-76971280 chr17:22521208-22521639"}        # MT+NUMT regions (MT 1st) 
+R=${4:-"chrM chr1:629084-634672 chr17:22521208-22521639"}                               # MT+NUMT regions (MT 1st) 
                                                                                         # chr1:76971223-76971280 & chr17:22521208-22521639 align to chrM 5'/3'
                                                                                         # chr1:629084-634672 : 5Kb NUMT
 
 #Examples: 
 #      downsampleSam.sh in out 
 #      downsampleSam.sh in out 10
+#      downsampleSam.sh in out 10 chrM
 #      downsampleSam.sh in out 10 "chrM chr1:629084-634672 chr1:76971223-76971280 chr17:22521208-22521639"
 
 test -s $IN.bam
-test -s $IN.bam.bai
+#test -s $IN.bam.bai
 
 ################################
 
 #downsample and index
 if [ ! -f  $OUT.bam ] ; then
-  samtools view -h $IN.bam $R -F 0x90C | filterSam.pl $R | downsampleSam.pl -max $MCOUNT | grep -v "^\@" | cut -f1 | sort | uniq -d   | samtools view -N /dev/stdin  $IN.bam -b > $OUT.bam
+  samtools view -h $IN.bam $R -F 0x90C | filterSam.pl $R | \
+    samtools view -b | samtools sort -n | samtools fixmate /dev/stdin /dev/stdout | samtools view -h | \
+    downsampleSam.pl -max $MCOUNT | grep -v "^\@" | cut -f1 | sort | uniq -d  | samtools view -N /dev/stdin  $IN.bam -b > $OUT.bam
   samtools index    $OUT.bam
   samtools idxstats $OUT.bam > $OUT.idxstats
 fi
