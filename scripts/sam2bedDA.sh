3ba427726d0e45f65da8e5007bd57e8e6ff765e5
dpuiu <dpuiu@jhu.edu>
2023-04-18 12:31:09 -0400

    git cleanup

--- scripts/sam2bedDA.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -eux
-
-samtools view $1.bam chrM | sam2bedDA.pl -min 1500  -max 15069 | sort -u > $2.da.bed
-cat $2.da.bed  |   cut -f1,3,9 | sort -V | uniq -c | perl -ane 'if($F[0]>=5) { print join "\t",@F[1,2,3,0]; print "\n"}' | bed2bed.pl > $2.da.count2_8.min5.bed
--- scripts/sam2bedSA.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -eux
-
-samtools view $1.bam chrM | sam2bedSA.pl -min_score 140 -min_pos 125 -max_pos 16444  | sort -u > $2.sa.bed
-cat $2.sa.bed  |   cut -f1,3,9 | sort -V | uniq -c | perl -ane 'if($F[0]>=5) { print join "\t",@F[1,2,3,0]; print "\n"}' | bed2bed.pl > $2.sa.count2_8.min5.bed
