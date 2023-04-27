d8b55f7ded894e8daf0e8955362aaf33d3efa809
Daniela Puiu <dpuiu@jhu.edu>
2023-04-27 16:18:26 -0400

    Update install_sysprerequisites.sh

--- scripts/install_sysprerequisites.sh
+++ scripts/install_sysprerequisites.sh
@@ -5,24 +5,29 @@ set -x
 
 # Program that checks/installs packages needed by bwa, samtools, bedtools ...
 
-# sudo privileges required !!!
+# run using sudo; sudo privileges required !!!
 
 ##############################################################################################################
 
-sudo -v
-if [ "$?" == 1 ] ; then
-  echo "ERROR: sudo permission required"
-  exit 1
+command -v apt-get
+if [ "$?" == 0 ] ; then
+  apt-get -y update && apt-get upgrade
+  apt-get install -y git wget default-jdk default-jre zlib1g libz-dev libncurses5-dev libbz2-dev pkg-config liblzma-dev build-essential unzip  parallel # make gcc
+  apt-get install -y python   # pip
+  apt-get install -y python-is-python3
 fi
 
-which apt-get
+command -v dnf
 if [ "$?" == 0 ] ; then
-  sudo apt-get -y update && apt-get upgrade
-  sudo apt-get install -y git wget default-jdk default-jre zlib1g libz-dev libncurses5-dev libbz2-dev pkg-config liblzma-dev python  build-essential unzip parallel # make gcc
+  dnf -y update
+  dnf install -y which nano git wget java-1.8.0-openjdk bzip2 gcc gcc-c++ zlib-devel ncurses-devel bzip2-devel xz-devel unzip perl perl-Data-Dumper perl-ExtUtils-MakeMaker perl-Test-Simple python3 python3-pip make
+  alternatives --install /usr/bin/python python /usr/bin/python3 60
 fi
 
-which yum
+command -v yum
 if [ "$?" == 0 ] ; then
-  sudo yum update
-  sudo yum install -y which nano git wget java-1.8.0-openjdk bzip2 gcc gcc-c++ zlib-devel ncurses-devel bzip2-devel xz-devel  unzip perl perl-Data-Dumper  perl-ExtUtils-MakeMaker perl-Test-Simple python parallel
+  yum -y update
+  #yum install -y which nano git wget java-1.8.0-openjdk bzip2 gcc gcc-c++ zlib-devel ncurses-devel bzip2-devel xz-devel  unzip perl perl-Data-Dumper  perl-ExtUtils-MakeMaker perl-Test-Simple python parallel
+  yum install -y which nano git wget java-1.8.0-openjdk bzip2 gcc gcc-c++ zlib-devel ncurses-devel bzip2-devel xz-devel  unzip perl perl-Data-Dumper  perl-ExtUtils-MakeMaker perl-Test-Simple python3 python3-pip make # removed parallel python
+  alternatives --install /usr/bin/python python /usr/bin/python3 60
 fi
