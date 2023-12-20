91806973832fe2c70a8889fd5af6feb79c2286c0
Daniela Puiu <dpuiu@jhu.edu>
2023-12-20 12:24:24 -0500

    Create mitohpc.sh

---
+++ scripts/mitohpc.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -ex
+
+##############################################################################################################
+
+# Script which launches the MitoHPC pipeline
+
+###############################################################################################################
+
+. $HP_SDIR/init.sh
+find $HP_ADIR/  -name "*.bam" -o -name "*.cram" -readable | ls2in.pl -out $HP_OUT | sort -V > $HP_IN
+$HP_SDIR/run.sh | bash
