82562c3e294fcdde9638b82a38d12fe581e1599b
Daniela Puiu <dpuiu@jhu.edu>
2023-12-21 10:25:30 -0500

    Update mitohpc.sh

--- scripts/mitohpc.sh
+++ scripts/mitohpc.sh
@@ -8,5 +8,5 @@ set -ex
 ###############################################################################################################
 
 . $HP_SDIR/init.sh
-find $HP_ADIR/  -name "*.bam" -o -name "*.cram" -readable | ls2in.pl -out $HP_OUT | sort -V > $HP_IN
+find $HP_ADIR/  -name "*.bam" -o -name "*.cram" -readable | ls2in.pl -out $HP_ODIR | sort -V > $HP_IN
 $HP_SDIR/run.sh | bash
